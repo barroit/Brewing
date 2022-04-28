997fb5eb646c4a7c688b85a64791cfc25638396f
Caishangqi <2470159730@qq.com>
2022-04-27 20:26:22 -0700

    remap

--- src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
+++ src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
@@ -1,5 +1,6 @@
 package minecraft.homeward.homewardpluginbrewing;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -9,9 +10,25 @@ public final class HomewardPluginBrewing extends JavaPlugin {
     //常量部分
     public final String PLUGIN_NAME = "酿酒";
 
+    //全局plugin 无需再次初始化但是必须在插件注册前
+    public static HomewardPluginBrewing plugin;
+
+    //覆写默认构造器
+    public HomewardPluginBrewing() {
+        plugin = this;
+    }
+
+    //储存配置文件类
+    public static FileConfiguration config;
+
 
     @Override
     public void onEnable() {
+
+        //注册默认Config,没有的话创建一个
+        saveDefaultConfig();
+        config = getConfig();
+
         getLogger().log(Level.INFO, "嘻嘻");
     }
 
