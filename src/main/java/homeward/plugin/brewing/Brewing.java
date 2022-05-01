59c90886dd7b176ca5480dfc8814c0e04de851a3
Ba1oretto <sunjiamu@outlook.com>
2022-05-02 07:06:59 +0900

    custom block

--- src/main/java/homeward/plugin/brewing/Brewing.java
+++ src/main/java/homeward/plugin/brewing/Brewing.java
@@ -1,20 +1,24 @@
 package homeward.plugin.brewing;
 
+import com.google.common.reflect.Reflection;
 import homeward.plugin.brewing.commands.MainCommand;
 import homeward.plugin.brewing.constants.BaseInfo;
+import homeward.plugin.brewing.listeners.BrewingBarrelListener;
 import homeward.plugin.brewing.utils.ConfigurationUtil;
 import me.mattstudios.mf.base.CommandManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.reflections.Reflections;
 
 import java.util.logging.Level;
 
 public final class Brewing extends JavaPlugin {
     private static Brewing plugin;
     private final CommandSender commandSender;
+    private final String packageName;
 
     /**
      *
@@ -33,6 +37,8 @@ public final class Brewing extends JavaPlugin {
         // register command here
         commandManager.register(new MainCommand());
 
+        this.registerListeners();
+
         this.onEnableMessage();
     }
 
@@ -40,12 +46,13 @@ public final class Brewing extends JavaPlugin {
         this.checkDepend();
         plugin = this;
         this.commandSender = super.getServer().getConsoleSender();
+        this.packageName = getClass().getPackageName();
         this.initializeConfiguration();
     }
 
     private void initializeConfiguration() {
         super.saveDefaultConfig();
-        new ConfigurationUtil(this, "data-resource", "recipes");
+        new ConfigurationUtil(this, "data-resource", "recipes", "block-surface");
     }
 
     private void onEnableMessage() {
@@ -59,5 +66,18 @@ public final class Brewing extends JavaPlugin {
             getLogger().log(Level.WARNING, "Could not find ItemsAdder! This plugin is required.");
             Bukkit.getPluginManager().disablePlugin(this);
         }
+    }
+
+    private void registerListeners() {
+        new Reflections(packageName + ".listeners").getSubTypesOf(Listener.class).forEach(v -> {
+            try {
+                if (v.getDeclaredConstructor().getParameterCount() == 0) {
+                    Listener listener = v.getDeclaredConstructor().newInstance();
+                    Bukkit.getServer().getPluginManager().registerEvents(listener, this);
+                }
+            } catch (Exception e) {
+                System.out.println("nothing happen");
+            }
+        });
     }
 }
---
+++ src/main/java/homeward/plugin/brewing/listeners/BrewingBarrelListener.java
@@ -0,0 +1,64 @@
+package homeward.plugin.brewing.listeners;
+
+import dev.lone.itemsadder.api.CustomBlock;
+import dev.lone.itemsadder.api.Events.CustomBlockPlaceEvent;
+import homeward.plugin.brewing.utils.ConfigurationUtil;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class BrewingBarrelListener implements Listener {
+
+    @EventHandler
+    public void onBarrelPlaced(CustomBlockPlaceEvent event) {
+        FileConfiguration configuration = ConfigurationUtil.get("block-surface");
+
+        String currentBlock = null;
+
+        String blockName = event.getNamespacedID().replaceAll("^.*:(.*)", "$1");
+
+        for (String v : configuration.getKeys(false)) {
+            if (v.equals(blockName)) {
+                currentBlock = v;
+            }
+        }
+
+        if (currentBlock == null) return;
+
+        ConfigurationSection section = configuration.getConfigurationSection(currentBlock);
+
+        String namespace = section.getString("namespace");
+
+        Player player = event.getPlayer();
+
+        Location location = event.getBlock().getLocation();
+        int yaw = (int) Math.floor(player.getLocation().getYaw());
+
+        CustomBlock customBlock;
+
+        if (inRange(yaw, 135, 180) || inRange(yaw, -180, -135)) {
+            customBlock = CustomBlock.getInstance(namespace + ":" + section.getString("south"));
+        } else if (inRange(yaw, -135, -45)) {
+            customBlock = CustomBlock.getInstance(namespace + ":" + section.getString("west"));
+        } else if (inRange(yaw, 0, 45) || inRange(yaw, -45, 0)) {
+            customBlock = CustomBlock.getInstance(namespace + ":" + section.getString("north"));
+        } else {
+            customBlock = CustomBlock.getInstance(namespace + ":" + section.getString("east"));
+        }
+
+        event.setCancelled(true);
+
+        customBlock.place(location);
+        customBlock.playPlaceSound();
+        player.swingMainHand();
+    }
+
+    private Boolean inRange(Integer target, Integer begin, Integer end) {
+        return target > begin && target < end;
+    }
+}
